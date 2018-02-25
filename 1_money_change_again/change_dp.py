515439c5dbd42fd95bf034ae7f7156a9e07ae6ac
FaiBarazi <f.barazi@gmail.com>
2018-02-25 13:40:48 -0500

    Implement dynamic programming for change money change

--- 1_money_change_again/change_dp.py
+++ 1_money_change_again/change_dp.py
@@ -1,9 +1,28 @@
 # Uses python3
 import sys
 
-def get_change(m):
-    #write your code here
-    return m // 4
+def get_min_change(change):
+    return min(change)
+
+def get_change(money):
+    """Return change matrix.
+
+    param:
+    money: int - money value
+    coins: list - coins
+    """
+    coins = [1, 3, 4]
+    change_matrix = [[0]]
+    if money == 0:
+        return 0
+    for i in range(1, money + 1):
+        row = []
+        for j in range(len(coins)):
+            if i >= coins[j]:
+                row.append(get_min_change(change_matrix[i - coins[j]]) + 1)
+        change_matrix.append(row)
+
+    return get_min_change(change_matrix[-1])
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
