a797a7d4ecf49acdec52e4834789bc770b863f4e
FaiBarazi <f.barazi@gmail.com>
2018-03-17 21:02:48 -0400

    Implement edit distance

--- 3_edit_distance/edit_distance.py
+++ 3_edit_distance/edit_distance.py
@@ -1,7 +1,23 @@
 # Uses python3
 def edit_distance(s, t):
-    #write your code here
-    return 0
+    """Calculate the minimum distance (Max alignment)."""
+    m = len(s) + 1
+    n = len(t) + 1
+    distance_table = {}
+    for i in range(m):
+        distance_table[i, 0] = i
+    for j in range(n):
+        distance_table[0, j] = j
+    for i in range(1, m):
+        for j in range(1, n):
+            if s[i - 1] == t[j - 1]:
+                mismatch = 0
+            else:
+                mismatch = 1
+            distance_table[i, j] = min(distance_table[i, j - 1] + 1, distance_table[i - 1, j] + 1, distance_table[i - 1, j - 1] + mismatch)
+    return distance_table[i, j]
+
+
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
